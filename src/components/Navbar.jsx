c4cd93e72eb0df3eae0ddd262e4d82fc0d051a1b
alperen-erol <alpereneroltr@gmail.com>
2023-07-27 10:40:25 +0300

    dropdown responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,16 +19,16 @@ const NavBar = () => {
         <Navbar.Brand ><Link className=" text-decoration-none" to="/">Movie App</Link></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Dropdown data-bs-theme="dark">
+          <Dropdown data-bs-theme="dark" className="dropdownmenu">
             <Dropdown.Toggle
               id="dropdown-button-dark-example1"
               variant="secondary"
-              className=" text-capitalize"
+              className=" text-capitalized "
             >
               {currentUser.displayName}
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
+            <Dropdown.Menu >
               <Dropdown.Item>
                 {" "}
                 <Link className=" text-decoration-none" to="login">Login</Link>{" "}
